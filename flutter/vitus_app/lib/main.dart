1d3385fef599ab3f5c574a13e39dd937dcd1f60f
vituscokr <37994226+vituscokr@users.noreply.github.com>
2023-10-05 11:13:24 +0900

    easy_location 테스트

--- flutter/vitus_app/lib/main.dart
+++ flutter/vitus_app/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:flutter_vitus/test_widget.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,14 +13,34 @@ import 'dart:math' show cos, sqrt, asin;
 
 import 'ui/page/map_view.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(child: MyApp(),
+        supportedLocales: const [
+          Locale('en',''),
+          Locale('ko',''),
+        ],
+        fallbackLocale: const Locale('en',''),
+        path: 'assets/translations',
+        startLocale: const Locale('en',''),
+        useFallbackTranslations: true,
+    )
+
+  );
+
+
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Flutter Maps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,7 +49,8 @@ class MyApp extends StatelessWidget {
 
       //TestAppFontSizePage(),
       //TestAppFontWeightPage(),
-      TestMainPage(),
+      //TestMainPage(),
+      TestWidget()
     );
   }
 }
